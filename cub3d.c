85ef23f19a9d7df36a84134e9d39916f49d35acd
sanghpar <k1990121@gmail.com>
2021-01-09 20:40:17 +0100

    tab

--- cub3d.c
+++ cub3d.c
@@ -2,26 +2,26 @@
 
 int main(int argc, char** argv)
 {
-    int fd;
+	int fd;
 
-    if (argc != 2)
-        return (-1);
-    else
-    {
-        if (!(fd = open(argv[1], O_RDONLY)))
-        {
-            printf("\nError in open\n");
-            return (-1);
-        }
-        if(cub_parse(fd) == -1)
-        {
-            printf("\nError in parsing\n");
-            return (-1);
-        }
-        if (setting() == -1)
-            return (-1);
-        mlx_loop_hook(game.mlx, cub3d_start, 0);
-        mlx_loop(game.mlx);
-    }
-    return (0);
+	if (argc != 2)
+		return (-1);
+	else
+	{
+		if (!(fd = open(argv[1], O_RDONLY)))
+		{
+			printf("\nError in open\n");
+			return (-1);
+		}
+		if(cub_parse(fd) == -1)
+		{
+			printf("\nError in parsing\n");
+			return (-1);
+		}
+		if (setting() == -1)
+			return (-1);
+		mlx_loop_hook(game.mlx, cub3d_start, 0);
+		mlx_loop(game.mlx);
+	}
+	return (0);
 }
